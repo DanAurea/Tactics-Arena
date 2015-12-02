6959004c3b98c25f49186a08a121675715f5d796
Nadilo <nadilo72@gmail.com>
2015-12-02 15:00:43 +0100

    ajout des cible du knight

--- src/units/knight.c
+++ src/units/knight.c
@@ -3,7 +3,8 @@
 /**
 * Initialise les cibles de l'unité
 */
-void knightInit(vector coordUnit, int noPlayer){
+void knightInit(vector coordUnit, int noPlayer)
+{
 	grid[coordUnit.x]grid[coordUnit.y].stat.HP = 50;
 	grid[coordUnit.x]grid[coordUnit.y].stat.POWER = 22;
 	grid[coordUnit.x]grid[coordUnit.y].stat.ARMOR = 0.25;
@@ -15,20 +16,38 @@ void knightInit(vector coordUnit, int noPlayer){
 	grid[coordUnit.x]grid[coordUnit.y].stat.RECOVERY = 1;
 	grid[coordUnit.x]grid[coordUnit.y].stat.MOVE_RANGE = 3;
 	
-	for(int i = 0; i < NB_MAX_EFFECT; i++){
+	for(int i = 0; i < NB_MAX_EFFECT; i++)
+	{
 		grid[coordUnit.x]grid[coordUnit.y].effect[i] = none;
 	}
 	
-	if(noPlayer == 1){
+	if(noPlayer == 1)
+	{
 		grid[coordUnit.x]grid[coordUnit.y].direct = north;
-	}else{
-		grid[coordUnit.x]grid[coordUnit.y].direct = west;
+	}
+	else
+	{
+		grid[coordUnit.x]grid[coordUnit.y].direct = south;
 	}
 }
 
 /**
 *	Initialise les cibles de l'unité	
 */
-void knightAttack(vector coordUnit){
-
+void knightTarget(vector coordUnit)
+{
+	for(int k=0;k<4;k++)
+	{
+		for(int i=-1;i<1;i++)
+		{
+			for(int j=-1;j<1;j++)
+			{
+				if(	abs(i)+abs(j)== 1)
+				{
+					grid[coordUnit.x]grid[coordUnit.y].unitTarget[0].x = coordUnit.x + i ;
+					grid[coordUnit.x]grid[coordUnit.y].unitTarget[0].y = coordUnit.y + j ;
+				}
+			}
+		}
+	}
 }
