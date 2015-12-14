a13d4f858552c71ff2546f89e8ebd907d8308693
Biardeau <biardeau.tristan@gmail.com>
2015-12-14 11:22:52 +0100

    Ajout menu aide

--- include/display/menu.h
+++ include/display/menu.h
@@ -2,3 +2,5 @@ void mainMenu();
 void gameMenu();
 void unitMenu(int choice);
 void unitList();
+void mainHelp();
+void helpUnit();
--- src/display/menu.c
+++ src/display/menu.c
@@ -12,7 +12,7 @@
  * Menu principal du jeu
  */
 void mainMenu(){
-puts(
+printf(
 "_________ _______  _______ __________________ _______  _______ " "\n"
 "\\__   __/(  ___  )(  ____ \\__   __/ \\__   __/(  ____ \\(  ____ \\ " "\n"
 "   ) (   | (   ) || (    \\/   ) (      ) (   | (    \\/| (    \\/" "\n"
@@ -41,11 +41,11 @@ puts(
 
         printf(" 1 - Nouvelle partie\n");
         printf(" 2 - Charger partie\n");
-        printf(" 3 - Test de gameMenu \n");
+        printf(" 3 - Aide jeu \n");
         printf(" 4 - Quitter\n");
         printf("Votre choix : ");   
         
-        choix = readLong();	// Check = 1 si chiffre est rentré, 0 sinon
+        choix = readLong();	// Choix = 1 si chiffre est rentré, 0 sinon
     	
         if(choix == 0) {
     		printf("Erreur : Veuillez rentrer un chiffre et non un caractère \n");
@@ -56,16 +56,229 @@ puts(
                 break;
             case 2: //loadGame();
                 break;
-            case 3: gameMenu();
+            case 3: mainHelp();
                 break;
             case 4: break;
             default: printf("Erreur: votre choix doit etre compris entre 1 et 4\n");
         }
 	
-    }while(choix != 4 || choix != 1);
+    }while(choix != 4);
     printf("A bientôt !");
     exit(1);
 }
+
+void mainHelp() {
+	int choix;
+	clearScreen();
+	color(red,"\n Bienvenue dans l'aide de notre jeu tactics arena ! \n");
+	do{
+		color(green, "\n Dîtes-moi, que voulez vous savoir ? \n");
+		printf( "  1 - Règles générales\n"
+			"  2 - Vocabulaire\n"
+			"  3 - Unités\n"
+			"  4 - Capacités spéciales\n"
+			"  5 - Effet de statut\n"
+			"  6 - Revenir au menu principal \n\n");
+		color(green, "Quel est votre choix ? : ");
+		choix = readLong();
+	    	
+		if(choix == 0) {
+	    		printf("Erreur : Veuillez rentrer un chiffre et non un caractère \n");
+	    	}
+		clearScreen();
+		switch(choix){
+
+				case 1 : color(cyan,"  Les conditions de victoire sont : \n");
+					puts(   "    -Toutes les unités adverses sont détruites ou immobilisé\n"
+						"    -L'opposant abandonne\n\n");
+					color(cyan,"  Les conditions de jeu nul sont : \n");
+					puts(	"    -Toutes les unités actives sont détruites ou immobilisé\n"
+						"    -OU les joueurs passent leurs tours 3 fois d’affilé\n"
+						"    -OU il n’y a eu aucun fight depuis les 30 derniers tours\n");
+					color(yellow,"  Info : Le joueur qui commence est défini aléatoirement par le serveur au début de chaque partie\n\n");
+					color(cyan,"  On peut contrôler une unité par tour pour :\n");
+					puts(	"    -Se déplacer\n"
+						"    -Et / ou attaquer\n"
+						"    -Et / ou changer de direction\n"
+						"    -Changer de direction doit être la dernière commande, si utilisé\n");
+					color(red,"    Le temps limite par tour est compris entre 1 et 2 minutes, qui dépend du nombre d’unité restantes\n\n");
+					color(cyan,"\n  Quand ce temps est dépassé:\n");
+					puts(   "    -Finis le tour automatiquement si au moins une commande a été effectué\n"
+						"    -Si aucune commande effectué, abandon automatique");
+					color(red, "    -Important : on ne peut pas revenir en arrière sur une action déjà effectué\n\n"); 
+
+				break;
+
+				case 2 : color(cyan, "  POWER : \n");
+					puts(   "    -Détermine combien de dégâts une unité peut infliger\n"
+						"    -OU détermine combien de HP peut redonner un soigneur\n\n");
+					color(cyan, "  ARMOR :\n");
+					puts(   "    -Détermine le % de dégâts qui sera absorbé et réduit\n"
+						"    -Ne réduit pas la valeur du soin ou du poison\n\n");
+
+					color(cyan, "  MOVEMENT RANGE :\n");
+					puts(   "    -Détermine le nombre maximum de cellules qu’une unité peut franchir par tour\n"
+						"    -Certaines unités alliées peuvent s’écarter pour laisser passer une autre unité \n\n");
+
+					color(cyan, "  RECOVERY TIME :\n");
+					puts(   "    -Détermine le nombre de tours qu’une unité doit attendre avant d’être réutiliser\n"
+						"    -Est divisé par 2 et est sous-arrondis si une unité se déplace mais n’attaque pas\n"
+						"    -OU est divisé par 2 et sur-arrondis si une unité attaque mais ne se déplace pas\n"
+						"    -OU est annulé si une unité change uniquement de direction\n\n");
+
+				break;
+
+				case 3 : helpUnit();
+				break;
+				
+				case 4 : color(cyan, "  TELEPORTATION \n"); 
+					 puts("    -Permet de ce déplacer vers une case libre sans être déranger par les obstacles ou les autres unités \n\n");
+
+					 color(cyan, "  ATTAQUES FURTIVES\n");
+					 puts("    -Ne peuvent êtres bloqués sauf par les BARRIERES \n\n");
+
+					 color(cyan, "  SOIN\n");
+					 puts("    -Restore les HP des unités\n\n");
+
+					 color(cyan, "  FOCUS\n");
+					 puts("    -Cause un effet continue tant que lanceur maintiens son sort \n"
+					      "    -Est supprimé si le lanceur est attaqué ou paralisé \n"
+					      "    -OU si la cible est détruite \n"
+					      "    -OU si le lanceur reçoit une nouvelle instruction\n\n");
+
+					color(cyan, "  BLOCK\n"); 
+					puts("    -Rend une attaque inefficace \n"
+					     "    -fixe 50% de bloquage sur les cotés \n"
+					     "    -Ne peut bloquer derrière \n\n");
+
+					color(red, "Les unités indestructibles ne peuvent être détruite ou endommagé \n\n");
+
+					color(cyan, "  \n  Étourdissement \n");
+					puts("    -Augmente d’un tour le RECOVERY TIME d’une unité\n"
+					     "    -N’a aucun effet si l’attaque est BLOCK\n\n");
+
+				break;
+
+				case 5 : color(cyan, "  WAITING\n");
+					puts("    -Indique qu’une unité est toujours en RECOVERY TIME et ne peut être commandé ce tour\n\n");
+
+					color(cyan, "  BARRIER\n");
+					puts("    -BLOCK toutes les attaques non-FOCUS et sorts de soin\n"
+					     "    -Est perdu si vous bougez, attaquez ou changez de direction\n"
+					     "    -Les unités sous l’effet BARRIER ne peuvent s’écarter pour laisser passer les unités alliées\n\n");
+
+					color(cyan, "  FOCUS\n");
+					puts("    -Les unités sous FOCUS ne peuvent bloquer\n"
+					     "    -Les unités sous l’effet FOCUS ne peuvent s’écarter pour laisser passer les unités\n\n");
+
+					color(cyan, "  PARALYSE\n");
+					puts("    -Ne peuvent être commandé\n"
+					     "    -Ne peuvent pas bloquer\n"
+					     "    -Ne peuvent s’écarter pour laisser passer les unités\n\n");
+
+					color(cyan, "  BONUS de PUISSANCE\n");
+					puts("    -Augmente ou Diminue la PUISSANCE temporairement\n"
+					     "    -Ne peut réduire la PUISSANCE sous 0\n"
+					     "    -N’a aucun effet sur les unité qui n’attaquent pas ou sur les soigneurs\n\n");
+
+					color(cyan, "  BONUS D’ARMURE\n");
+					puts("    -Augmente ou Diminue l’ARMURE temporairement\n"
+					     "    -Ne peut réduire l’ARMURE sous 0\n"
+					     "    -Ne peut augmenté l’ARMURE au dessus de 100\n\n");
+
+					color(cyan, "  POISON\n");
+					puts("    -Ne peut ATTAQUER\n"
+					     "    -ET ne peut s’écarter pour laisser le passage au unités allier\n"
+					     "    -ET pert 4 HP à char TOURS\n");
+
+					color(red, "Tout les Effets de Statuts peuvent se cumuler !\n\n");
+
+
+		
+		}
+	}while(choix != 6);
+}
+
+
+
+void helpUnit() {
+	int choix;
+	color(green, "Veuillez choisir une unité dont vous voulez connaître des informations : \n");
+	printf( "1 - Knight\n"
+		"2 - Scout\n"
+		"3 - Assassin\n"
+		"4 - Cleric\n"
+		"5 - Pyromancer\n"
+		"6 - Enchantress\n"
+		"7 - Dragonborn\n"
+		"8 - Dark Witch\n"
+		"9 - Lightning Totem\n"
+		"10 - Barrier Totem\n"
+		"11 - Mud Golem\n"
+		"12 - Golem Ambusher\n"
+		"13 - Frost Golem\n"
+		"14 - Stone Golem\n"
+		"15 - Dragon Tyrant\n"
+		"16 - Berserker\n"
+		"17 - Beast Rider\n"
+		"18 - Poison Wisp\n"
+		"19 - Furgon\n"
+		"20 - Retour menu principal\n\n");
+	color(green, "Quel est votre choix ? : ");
+	choix = readLong();
+    	
+        if(choix == 0) {
+    		printf("Erreur : Veuillez rentrer un chiffre et non un caractère \n");
+    	}
+	switch(choix){
+
+		case 1 : puts("\nCaractéristiques du Knight : \n"
+			 	"HP : 50\n"
+				"Power : 22\n" 
+				"Armor : 25%\n"
+				"Blocking : 80% Devant 40% Côté\n"
+				"Recovery : 1\n"
+				"Movement : 3\n"
+				"Information(s) supplémentaire(s) : Pas plus de 3 Knight sur le champ de bataille.");
+		break;
+
+		case 2 : puts("\nCaractéristiques du Scout : \n"
+			 	"HP : 40\n"
+				"Power : 18\n" 
+				"Armor : 8%\n"
+				"Blocking : 60% Devant 30% Côté\n"
+				"Recovery : 2\n"
+				"Movement : 4\n"
+				"Information(s) supplémentaire(s) : Pas plus de 2 Scout sur le champ de bataille.");
+		break;
+
+		case 3 : puts("\nCaractéristiques de l'Assassin : \n"
+			 	"HP : 35\n"
+				"Power : 18\n" 
+				"Armor : 12%\n"
+				"Blocking : 70% Devant 35% Côté\n"
+				"Recovery : 1\n"
+				"Movement : 4\n"
+				"Information(s) supplémentaire(s) : Quand l'assassin possède moins de 5 HP, il peut faire le kamikaze");
+		break;
+
+		case 3 : puts("\nCaractéristiques du Cleric : \n"
+			 	"HP : 24\n"
+				"Power : 12 de soin\n" 
+				"Armor : Aucune\n"
+				"Blocking : Aucun\n"
+				"Recovery : 5\n"
+				"Movement : 3\n"
+				"Information(s) supplémentaire(s) : Soigne tout les alliés. Le sort de soin ne peut être bloqué sauf par les barrières.");
+		break;
+
+
+
+	}
+
+}
+
+
 
 /**
  * Menu du joueur lors de la partie
