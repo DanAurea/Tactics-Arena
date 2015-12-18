43590384460391d7569e2288b04a03efe2772d20
Brandon / DanAurea <laochi.bc@gmail.com>
2015-12-18 13:47:18 +0100

    Nettoyage dépôt

--- include/menu.h
+++
@@ -1,6 +0,0 @@
-void mainMenu();
-char* getNameUnit(unitName unit);
-void printNameUnit(unitName unit);
-void gameMenu(unitAction movable, unitAction attackable);
-void unitMenu(int choice, unitAction movable, unitAction attackable);
-void unitList();
